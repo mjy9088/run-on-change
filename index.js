26829e26fc8d247e5e8b535f052ec8a551499744
맹주영 <mjy9088@naver.com>
2019-08-26 23:15:02 +0900

    support files in directory

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const child_process = require('child_process');
 
 var fn2d = new Map();
 
-function listener(eventType, fileName) {
-    let command = fn2d.get(fileName);
-    if(!command) console.log('An error has occurred.');
-    command = fn2d.get(fileName).map(value => typeof value === 'number' ? fileName : String(value));
-    let child = child_process.spawn(command.shift(), command, {
-        stdio: ['ignore', 'pipe', 'pipe'],
-        windowsHide: true
-    });
-    child.on('error', function(err) {
-        console.log('ERROR : Failed to run: ' + err);
-    });
-    child.stdout.on('data', (data) => console.log(fileName + " : " + data));
-    child.stderr.on('data', (data) => console.log(fileName + " : " + data));
+function getListener(fileName, comm) {
+    return () => {
+        let command = comm.map(value => typeof value === 'number' ? fileName : String(value));
+        let child = child_process.spawn(command.shift(), command, {
+            stdio: ['ignore', 'pipe', 'pipe'],
+            windowsHide: true
+        });
+        child.on('error', function(err) {
+            console.log('ERROR : Failed to run: ' + err);
+        });
+        child.stdout.on('data', (data) => console.log(fileName + " : " + data));
+        child.stderr.on('data', (data) => console.log(fileName + " : " + data));
+    };
 }
 
 fs.readFile('run-on-change.json', (err, data) => {
@@ -27,9 +27,8 @@ fs.readFile('run-on-change.json', (err, data) => {
     for(let i = 0; i < list.length; i++) {
         if(!list[i] instanceof Array) throw 'Invalid run-on-change.json file';
         for(let j = 1; j < list[i].length; j++) {
-            fn2d.set(list[i][j], list[i][0]);
             try {
-                fs.watch(list[i][j], listener);
+                fs.watch(list[i][j], getListener(list[i][j], list[i][0]));
             } catch (error) {
                 console.log('ERROR : Failed to watch file: ' + list[i][j]);
             }
